f77f6fb4f76cfdd9f59a235ebc14ed714ad5fe40
Kim Dae Young <(dafakim@gmail.com)>
2022-04-07 17:54:42 +0900

    .\start_control.py

--- start_control.py
+++ start_control.py
@@ -18,70 +18,90 @@ HUMHIGH = 85
 HUMLOW = 80
 AIRWASHTIME = 5
 
-def _process_temp(msg):
-    msg = msg.split(',')
+def _process_temp(location, msg):
     #time = datetime.strftime(datetime.now(), "%Y-%M-%D %H:%M:%S")
-    json_body = [
-        {
-            "measurement": "temperature",
-            "time": datetime.now(timezone('Asia/Seoul')),
-            "fields": {
-                "T1": float(msg[0]),
-                "T2": float(msg[1]),
-                "action": False
+    if len(msg) > 1:
+        json_body = [
+            {
+                "measurement": "temperature",
+                "time": datetime.now(timezone('Asia/Seoul')),
+                "fields": {
+                    "T1": float(msg[0]),
+                    "T2": float(msg[1]),
+                    "action": False
+                }
             }
-        }
-    ]
-    heater = heater_miot.HeaterMiot(ip=os.getenv('HEATER_IP'), token=os.getenv('HEATER_TOKEN'))
-    is_on = heater.status().is_on
-    total = 0
-    for i in range(len(msg)):
-        logging.debug("Sensor: {}, Value: {}".format(i, msg[i]))
-        total += float(msg[i])
-    avg = total/len(msg)
-    if avg > TEMPHIGH and is_on:
-        heater.off()
-    elif avg < TEMPLOW and not is_on:
-        heater.on()
-    json_body[0]["fields"]["action"] = heater.status().is_on
-    print(dbm.db_insert("hyoja", json_body))
+        ]
+        heater = heater_miot.HeaterMiot(ip=os.getenv('HEATER_IP'), token=os.getenv('HEATER_TOKEN'))
+        is_on = heater.status().is_on
+        total = 0
+        for i in range(len(msg)):
+            logging.debug("Sensor: {}, Value: {}".format(i, msg[i]))
+            total += float(msg[i])
+        avg = total/len(msg)
+        if avg > TEMPHIGH and is_on:
+            heater.off()
+        elif avg < TEMPLOW and not is_on:
+            heater.on()
+        json_body[0]["fields"]["action"] = heater.status().is_on
+    else:
+        json_body = [
+            {
+                "measurement": "temperature",
+                "time": datetime.now(timezone('Asia/Seoul')),
+                "fields": {
+                    "T1": float(msg[0])
+                }
+            }
+        ]
+    print(dbm.db_insert(location, json_body))
 
-def _process_humi(msg):
-    msg = msg.split(',')
+def _process_humi(location, msg):
     #time = datetime.strftime(datetime.now(), "%Y-%M-%D %H:%M:%S")
-    json_body = [
-        {
-            "measurement": "humidity",
-            "time": datetime.now(timezone('Asia/Seoul')),
-            "fields": {
-                "H1": float(msg[0]),
-                "H2": float(msg[1]),
-                "action": False
+    if len(msg) > 1:
+        json_body = [
+            {
+                "measurement": "humidity",
+                "time": datetime.now(timezone('Asia/Seoul')),
+                "fields": {
+                    "H1": float(msg[0]),
+                    "H2": float(msg[1]),
+                    "action": False
+                }
             }
-        }
-    ]
-    humidifier = airhumidifier_mjjsq.AirHumidifierMjjsq(ip=os.getenv('HUMIDIFIER_IP'), token=os.getenv('HUMIDIFIER_TOKEN'))
-    is_on = humidifier.status().is_on
-    if humidifier.status().no_water:
-        logging.error("HUMIDIFIER NO WATER, PLEASE FILL ASAP")
-        if is_on:
+        ]
+        humidifier = airhumidifier_mjjsq.AirHumidifierMjjsq(ip=os.getenv('HUMIDIFIER_IP'), token=os.getenv('HUMIDIFIER_TOKEN'))
+        is_on = humidifier.status().is_on
+        if humidifier.status().no_water:
+            logging.error("HUMIDIFIER NO WATER, PLEASE FILL ASAP")
+            if is_on:
+                humidifier.off()
+            else:
+                pass
+            return -1
+        total = 0
+        for i in range(len(msg)):
+            logging.debug("Sensor: {}, Value: {}".format(i, msg[i]))
+            total += float(msg[i])
+        avg = total/len(msg)
+        if avg > HUMHIGH and is_on:
+            # turn off humidifier
             humidifier.off()
-        else:
-            pass
-        return -1
-    total = 0
-    for i in range(len(msg)):
-        logging.debug("Sensor: {}, Value: {}".format(i, msg[i]))
-        total += float(msg[i])
-    avg = total/len(msg)
-    if avg > HUMHIGH and is_on:
-        # turn off humidifier
-        humidifier.off()
-    elif avg < HUMLOW and not is_on:
-        # turn on humidifier
-        humidifier.on()
-    json_body[0]["fields"]["action"] = humidifier.status().is_on
-    dbm.db_insert("hyoja", json_body)
+        elif avg < HUMLOW and not is_on:
+            # turn on humidifier
+            humidifier.on()
+        json_body[0]["fields"]["action"] = humidifier.status().is_on
+    else:
+        json_body = [
+            {
+                "measurement": "humidity",
+                "time": datetime.now(timezone('Asia/Seoul')),
+                "fields": {
+                    "H1": float(msg[0])
+                }
+            }
+        ]
+    dbm.db_insert(location, json_body)
 
 def _process_airwash():
     device = {
@@ -116,16 +136,19 @@ def _on_message(client, userdata, msg):
     sensor_type = topic[1]
     decoded_msg = msg.payload.decode('utf-8')
     #logging.info("{}\nLOCATION: {}\nSENSOR: {}\nPAYLOAD: {}".format(datetime.now(timezone('Asia/Seoul')), location, sensor_type, decoded_msg))
-    msg = decoded_msg.split(',')
-    if msg[0] == msg[1]:
-        if int(float(msg[0])) == 0:
-            print("Zero Data")
-            #sn.send_notification("Zero Data Notification", "Receieved 0 at following sensor\nLOCATION: {}\nSENSOR: {}".format(location, sensor_type))
+    if ',' in decoded_msg:
+        split_msg = decoded_msg.split(',')
+        if split_msg[0] == split_msg[1]:
+            if int(float(split_msg[0])) == 0:
+                print("Zero Data")
+                #sn.send_notification("Zero Data Notification", "Receieved 0 at following sensor\nLOCATION: {}\nSENSOR: {}".format(location, sensor_type))
+    else:
+        split_msg = decoded_msg
     # disable temperature humidity controls until setup finished
     if "temperature" in sensor_type:
-        _process_temp(decoded_msg)
+        _process_temp(location, split_msg)
     elif "humidity" in sensor_type:
-        _process_humi(decoded_msg)
+        _process_humi(location, split_msg)
     else:
         pass
     _process_airwash()
